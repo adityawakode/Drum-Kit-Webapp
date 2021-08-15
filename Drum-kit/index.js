41bf62209661a930119454d9a114eda6995a83cc
Aditya Wakode <64493902+adityawakode@users.noreply.github.com>
2021-08-15 11:29:28 +0530

    Add files via upload

---
+++ Drum-kit/index.js
@@ -0,0 +1,13 @@
+window.addEventListener('load',()=>{
+     const sounds= document.querySelectorAll('.sound');
+     const pads= document.querySelectorAll('.drum-kit div');
+
+     //sound
+  pads.forEach((pad,index)=>{
+pad.addEventListener('click',function(){
+    sounds[index].currentTime=0;
+    sounds[index].play();
+});
+  });
+        
+});
